910b21c7c26de775acc3f961b0c3b3fc1219464a
arch-spatula <shkim516@naver.com>
2023-05-29 19:26:38 +0900

    chore: msw 설치 및 설정 #2

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  fetch("http://localhost:5173/todos")
+    .then((response) => response.json())
+    .then((data) => setCount(data));
 
-  return <h1>hello world!</h1>;
+  return <h1>hello world! {count}</h1>;
 }
 
 export default App;
