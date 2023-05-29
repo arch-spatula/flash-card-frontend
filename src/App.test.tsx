32f4f0d0f4c6d52932622bc88b608f59fb8d7038
arch-spatula <shkim516@naver.com>
2023-05-29 19:57:24 +0900

    chore: user-event 설치 #2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
+import user from "@testing-library/user-event";
 import App from "./App";
 
 describe("App", () => {
-  it("should renders hello world!", () => {
+  it("should renders hello world!", async () => {
     // Arrange
+    user.setup();
     render(<App />);
 
     // Act(선택적)
--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  fetch("http://localhost:5173/todos")
-    .then((response) => response.json())
-    .then((data) => setCount(data));
+  // fetch("http://localhost:5173/todos")
+  //   .then((response) => response.json())
+  //   .then((data) => setCount(data));
 
   return <h1>hello world! {count}</h1>;
 }
