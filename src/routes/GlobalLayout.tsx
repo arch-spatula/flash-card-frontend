79e23b056db91ab0b9ec50eb05e03335f8992630
arch-spatula <shkim516@naver.com>
2023-05-30 18:57:57 +0900

    chore: 스타일링 관심사 분리 #2

--- src/routes/GlobalLayout.tsx
+++ src/routes/GlobalLayout.tsx
@@ -1,35 +1,9 @@
-import { Global, css } from '@emotion/react';
+import { Global } from '@emotion/react';
+import GlobalStyle from '../styles/Reset';
 
 /**
  * @see https://github.com/WANTED-TEAM03/pre-onboarding-10th-1-3/blob/main/src/routes/_globalLayout.tsx
- *
  */
-
-const GlobalStyle = css`
-  * {
-    padding: 0;
-    border: 0;
-    margin: 0;
-    font-size: 16px;
-  }
-
-  a {
-    text-decoration: none;
-  }
-
-  li {
-    list-style: none;
-  }
-
-  input {
-    appearance: none;
-    outline: none;
-  }
-
-  table {
-    border-collapse: collapse;
-  }
-`;
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
