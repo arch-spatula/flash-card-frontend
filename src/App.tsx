a54b7cc2b7a38129e94ebb6ce5b3faa6695ef9b5
arch-spatula <shkim516@naver.com>
2023-05-31 20:20:21 +0900

    feat: origin 허용 확인 #8

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { signInAPI } from './api/authClient';
 import Router from './routes/Routes';
 
 function App() {
-  return <Router />;
+  const login = async () => {
+    const foo = await signInAPI('username@email.com', '12345678');
+    console.log(foo);
+    return foo;
+  };
+
+  return (
+    <>
+      <button onClick={login}>로그인</button>
+      <Router />
+    </>
+  );
 }
 
 export default App;
--- src/api/authClient.ts
+++ src/api/authClient.ts
@@ -1,14 +1,17 @@
-import { AxiosError } from 'axios';
+import { AxiosError, AxiosResponse } from 'axios';
 import axiosClient from './AxiosClient';
 import { API_URLS, BASE_URL } from '../constant/config';
 
 async function signInAPI(email: string, password: string) {
   try {
-    const res = await axiosClient.post(BASE_URL + API_URLS.SIGN_IN, {
-      email,
-      password,
-    });
-    return res;
+    const res: AxiosResponse<{ email: string }> = await axiosClient.post(
+      BASE_URL + API_URLS.SIGN_IN,
+      {
+        email,
+        password,
+      }
+    );
+    return res.data;
   } catch (error) {
     if (error instanceof AxiosError) {
       return error;
