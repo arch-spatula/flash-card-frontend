a0c3922698f52c45e4933a8e8340978053060839
arch-spatula <shkim516@naver.com>
2023-05-31 17:02:31 +0900

    feat: API, 라우팅 경로 상수 추가 #8

---
+++ src/constant/config.ts
@@ -0,0 +1,14 @@
+export const BASE_URL = 'https://flash-card-backend.deno.dev/api';
+
+export const API_URLS = {
+  CARDS: '/card',
+  SIGN_IN: '/auth/signin',
+  SIGN_UP: '/auth/signup',
+};
+
+export const ROUTE_PATHS = {
+  WELCOME: '/',
+  SIGN_IN: '/signin',
+  SIGN_UP: '/signup',
+  CARDS: '/cards',
+};
