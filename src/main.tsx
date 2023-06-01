b367b3304584d2142e79218cd8896980a6d316eb
arch-spatula <shkim516@naver.com>
2023-06-01 15:34:58 +0900

    feat: mocking 구현 보류 #8

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { worker } from './mocks/worker.ts';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '@emotion/react';
 import theme from './styles/theme.ts';
-
-if (process.env.NODE_ENV === 'development') {
-  /**
-   * api가 안정화 되면 아래 설정 {onUnhandledRequest: 'bypass'}은 해제합니다.
-   * @see https://stackoverflow.com/questions/68024935/msw-logging-warnings-for-unhandled-supertest-requests
-   */
-  worker.start({ onUnhandledRequest: 'bypass' });
-}
 
 const queryClient = new QueryClient();
 
