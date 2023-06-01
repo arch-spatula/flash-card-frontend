8670d9583352a7f75ea48575ba7dcb7ce1c0c655
arch-spatula <shkim516@naver.com>
2023-06-01 17:25:35 +0900

    feat: Nav 컴포넌트 추가 #7

---
+++ src/Components/Navbar/index.tsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <nav>home</nav>;
+}
+
+export { Navbar };
--- src/routes/GlobalLayout.tsx
+++ src/routes/GlobalLayout.tsx
@@ -1,15 +1,15 @@
 import { Global } from '@emotion/react';
 import GlobalStyle from '../styles/Reset';
+import { Navbar } from '../Components';
 
 /**
  * @see https://github.com/WANTED-TEAM03/pre-onboarding-10th-1-3/blob/main/src/routes/_globalLayout.tsx
  */
-
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Global styles={GlobalStyle} />
-      <nav>nav</nav>
+      <Navbar />
       {children}
     </>
   );
