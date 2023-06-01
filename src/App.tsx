9155d0aa1c7e4d047cf90ec518289d08e6c8119b
arch-spatula <shkim516@naver.com>
2023-06-01 17:26:09 +0900

    refactor: 테스트 로그인 버튼 제거 #7

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { signInAPI } from './api/authClient';
 import Router from './routes/Routes';
 
 function App() {
-  const login = async () => {
-    const foo = await signInAPI('username@email.com', '12345678');
-    console.log(foo);
-    return foo;
-  };
-
-  return (
-    <>
-      <button onClick={login}>로그인</button>
-      <Router />
-    </>
-  );
+  return <Router />;
 }
 
 export default App;
