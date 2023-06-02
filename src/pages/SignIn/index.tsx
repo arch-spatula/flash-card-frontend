f07cedf65baf79c349a8c5de510656ba8f95d62e
arch-spatula <shkim516@naver.com>
2023-06-02 15:46:15 +0900

    refactor: 동사를 접두어로 리팩토링 #16

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,22 @@
+import { Button, Input } from '../../Components';
+import { useInput } from '../../hooks';
+
 function SignIn() {
-  return <div>Sign In</div>;
+  const { inputVal: emailValue, changeInputVal: changeEmail } = useInput();
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <Input type="email" onChange={changeEmail} value={emailValue} />
+      <Button
+        onClick={(e) => {
+          console.log(e.target);
+        }}
+      >
+        Sign In
+      </Button>
+    </div>
+  );
 }
 
 export { SignIn };
