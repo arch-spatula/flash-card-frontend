ba2688bcd4602505b5c4e1265026e951ea11bbdb
arch-spatula <shkim516@naver.com>
2023-06-02 17:24:25 +0900

    feat: 로그인 기능 구현 #16

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,20 +1,30 @@
 import { Button, Input } from '../../Components';
+import { signInAPI } from '../../api/authClient';
 import { useInput } from '../../hooks';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE_PATHS } from '../../constant/config';
 
 function SignIn() {
   const { inputVal: emailValue, changeInputVal: changeEmail } = useInput();
+  const { inputVal: passwordValue, changeInputVal: changePassword } =
+    useInput();
+  const navigate = useNavigate();
+
+  const signIn = async () => {
+    try {
+      const res = await signInAPI(emailValue, passwordValue);
+      if (res.email) navigate(ROUTE_PATHS.CARDS);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div>
       <h1>Sign In</h1>
       <Input type="email" onChange={changeEmail} value={emailValue} />
-      <Button
-        onClick={(e) => {
-          console.log(e.target);
-        }}
-      >
-        Sign In
-      </Button>
+      <Input type="password" onChange={changePassword} value={passwordValue} />
+      <Button onClick={signIn}>Sign In</Button>
     </div>
   );
 }
