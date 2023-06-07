9b19a460b11c470170c580d368c1b922fb4230ef
arch-spatula <shkim516@naver.com>
2023-06-07 17:01:17 +0900

    refactor: import queryClient from libs #25

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '@emotion/react';
 import theme from './styles/theme.ts';
-
-const queryClient = new QueryClient();
+import queryClient from './libs/queryClient.ts';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
