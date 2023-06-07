37862e985a5b61cb59c255e17111e471f6155d4c
arch-spatula <shkim516@naver.com>
2023-06-07 13:34:27 +0900

    refactor: add type to semantic and gray colors #22

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -4,18 +4,21 @@ declare module '@emotion/react' {
   export interface Theme {
     color: {
       red: string;
+      yellow: string;
+      green: string;
+      blue: string;
       black: string;
       white: string;
-      'gray-050': string;
-      'gray-100': string;
-      'gray-200 ': string;
-      'gray-300 ': string;
-      'gray-400': string;
-      'gray-500': string;
-      'gray-600': string;
-      'gray-700': string;
-      'gray-800': string;
-      'gray-900': string;
+      gray050: string;
+      gray100: string;
+      gray200: string;
+      gray300: string;
+      gray400: string;
+      gray500: string;
+      gray600: string;
+      gray700: string;
+      gray800: string;
+      gray900: string;
     };
     fonts: {};
   }
