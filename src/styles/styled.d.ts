cb9051c02f4b3af0ae5e8b55bce1874fbee4d48d
arch-spatula <shkim516@naver.com>
2023-06-07 13:15:01 +0900

    feat: add gray colors #22

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -4,6 +4,18 @@ declare module '@emotion/react' {
   export interface Theme {
     color: {
       red: string;
+      black: string;
+      white: string;
+      'gray-050': string;
+      'gray-100': string;
+      'gray-200 ': string;
+      'gray-300 ': string;
+      'gray-400': string;
+      'gray-500': string;
+      'gray-600': string;
+      'gray-700': string;
+      'gray-800': string;
+      'gray-900': string;
     };
     fonts: {};
   }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,18 @@
 const theme = {
   colors: {
     red: 'ff0000',
+    black: '#000000',
+    white: '#ffffff',
+    'gray-050': '#F9FAFB',
+    'gray-100': '#F3F4F6',
+    'gray-200 ': '#E5E7EB',
+    'gray-300 ': '#D1D5DB',
+    'gray-400': '#9CA3AF',
+    'gray-500': '#6B7280',
+    'gray-600': '#6B7280',
+    'gray-700': '#374151',
+    'gray-800': '#1F2937',
+    'gray-900': '#111827',
   },
   fonts: {},
 };
