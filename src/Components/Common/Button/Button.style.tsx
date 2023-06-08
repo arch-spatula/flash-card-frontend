42eddf50882acc43d60946a8943a99a5a5330197
arch-spatula <shkim516@naver.com>
2023-06-08 21:14:48 +0900

    feat: change color when button is disabled #24

--- src/Components/Common/Button/Button.style.tsx
+++ src/Components/Common/Button/Button.style.tsx
@@ -4,7 +4,8 @@ export const ButtonWrapper = styled.button`
   ${(props) => props.theme.fonts.body16Regular}
   border-radius: 0.25rem;
   border: none;
-  background-color: ${(props) => props.theme.colors.green};
+  background-color: ${(props) =>
+    props.disabled ? props.theme.colors.gray400 : props.theme.colors.green};
   padding: 0.25rem 0.75rem;
   color: ${(props) => props.theme.colors.white};
 `;
