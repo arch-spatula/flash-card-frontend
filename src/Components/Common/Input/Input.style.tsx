a680b066ebe8c97fba9b32e8260308b52a17eb42
arch-spatula <shkim516@naver.com>
2023-06-08 17:56:52 +0900

    refactor: remove type from HelperText #24

--- src/Components/Common/Input/Input.style.tsx
+++ src/Components/Common/Input/Input.style.tsx
@@ -2,11 +2,7 @@ import styled from '@emotion/styled';
 
 export const InputWrapper = styled.input``;
 
-type HelperTextProps = {
-  visibility?: boolean;
-};
-
-export const HelperText = styled.p<HelperTextProps>`
+export const HelperText = styled.p`
   ${(props) => props.theme.fonts.caption12Regular}
   min-height: 1.25rem;
 `;
