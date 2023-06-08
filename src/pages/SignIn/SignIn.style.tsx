26494b1013637cd2492bb08a730a378fa7d15f40
arch-spatula <shkim516@naver.com>
2023-06-08 17:45:35 +0900

    feat: Center SignIn Page main #24

---
+++ src/pages/SignIn/SignIn.style.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(100vh - 4rem);
+`;
