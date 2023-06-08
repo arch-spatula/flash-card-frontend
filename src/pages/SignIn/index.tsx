eaa8ec335a0bb4d8b8d49316462b66fe0da3399f
arch-spatula <shkim516@naver.com>
2023-06-08 17:52:25 +0900

    feat: add helper text for user feedback #24

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,29 +3,57 @@ import { signInAPI } from '../../api/authClient';
 import { useInput } from '../../hooks';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_PATHS } from '../../constant/config';
+import { useState } from 'react';
+import { MainContainer } from './SignIn.style';
 
 function SignIn() {
   const { inputVal: emailValue, changeInputVal: changeEmail } = useInput();
   const { inputVal: passwordValue, changeInputVal: changePassword } =
     useInput();
   const navigate = useNavigate();
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
 
   const signIn = async () => {
     try {
+      setEmailError('');
+      setPasswordError('');
       const res = await signInAPI(emailValue, passwordValue);
-      if (res.email) navigate(ROUTE_PATHS.CARDS);
+      if (res?.msg === 'Error: 비밀번호가 일치하지 않습니다.')
+        throw new Error('비밀번호가 일치하지 않습니다.');
+      if (res?.msg === 'Error: 이메일이 없습니다.')
+        throw new Error('이메일이 없습니다.');
+
+      if (res?.success) {
+        navigate(ROUTE_PATHS.CARDS);
+      }
     } catch (error) {
-      console.log(error);
+      const err = error as Error;
+      if (err.message === '이메일이 없습니다.') {
+        setEmailError('이메일이 없습니다.');
+      }
+      if (err.message === '비밀번호가 일치하지 않습니다.')
+        setPasswordError('비밀번호가 일치하지 않습니다.');
     }
   };
 
   return (
-    <div>
+    <MainContainer>
       <h1>Sign In</h1>
-      <Input type="email" onChange={changeEmail} value={emailValue} />
-      <Input type="password" onChange={changePassword} value={passwordValue} />
+      <Input
+        type="email"
+        onChange={changeEmail}
+        value={emailValue}
+        helperText={emailError}
+      />
+      <Input
+        type="password"
+        onChange={changePassword}
+        value={passwordValue}
+        helperText={passwordError}
+      />
       <Button onClick={signIn}>Sign In</Button>
-    </div>
+    </MainContainer>
   );
 }
 
