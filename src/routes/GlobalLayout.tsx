d7c852de336df36dac0cdfe968dbbefce8d5c6d5
arch-spatula <shkim516@naver.com>
2023-06-08 12:56:10 +0900

    feat: add MainContainer to center page #24

--- src/routes/GlobalLayout.tsx
+++ src/routes/GlobalLayout.tsx
@@ -2,6 +2,7 @@ import { Global } from '@emotion/react';
 import GlobalStyle from '../styles/Reset';
 import { Navbar } from '../Components';
 import { Outlet } from 'react-router-dom';
+import styled from '@emotion/styled';
 
 /**
  * @see https://github.com/WANTED-TEAM03/pre-onboarding-10th-1-3/blob/main/src/routes/_globalLayout.tsx
@@ -11,9 +12,17 @@ function Layout() {
     <>
       <Global styles={GlobalStyle} />
       <Navbar />
-      <Outlet />
+      <MainContainer>
+        <Outlet />
+      </MainContainer>
     </>
   );
 }
+
+const MainContainer = styled.main`
+  width: 82.5rem;
+  margin: auto;
+  height: calc(100vh - 4rem);
+`;
 
 export default Layout;
