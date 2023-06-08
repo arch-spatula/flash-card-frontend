3ee1a2603fd6bd16852c454e0901c0cf4e46d2ae
arch-spatula <shkim516@naver.com>
2023-06-08 20:10:50 +0900

    chore: configure BrowserRouter #24

--- src/libs/test-utils/AllTheProviders.tsx
+++ src/libs/test-utils/AllTheProviders.tsx
@@ -2,13 +2,16 @@ import { ThemeProvider } from '@emotion/react';
 import theme from '../../styles/theme';
 import queryClient from '../queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
 
 type AllProvidersProps = { children: React.ReactNode };
 
 export default function AllProviders({ children }: AllProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
