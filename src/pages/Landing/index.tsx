71df7899afddf1bec92a4812e706045e9679fe1b
arch-spatula <shkim516@naver.com>
2023-06-09 16:41:33 +0900

    feat: remove useLogin temporary function #24

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,11 +1,10 @@
 import { useNavigate } from 'react-router-dom';
-import { Button } from '../../Components';
 import { useLogin } from '../../hooks';
 import { ROUTE_PATHS } from '../../constant/config';
 import { useEffect } from 'react';
 
 function Landing() {
-  const { login, logout, token } = useLogin();
+  const { token } = useLogin();
   const navigate = useNavigate();
   useEffect(() => {
     if (token) {
@@ -16,8 +15,6 @@ function Landing() {
   return (
     <div>
       <h1>Welcome</h1>
-      <Button onClick={login}>login</Button>
-      <Button onClick={logout}>logout</Button>
     </div>
   );
 }
