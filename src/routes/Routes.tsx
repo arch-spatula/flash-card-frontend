d34eed70f95ec7142ad674d0e14b778250426d2f
arch-spatula <shkim516@naver.com>
2023-06-09 14:50:14 +0900

    feat: add lazy loading to page routes #30

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,15 +1,29 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './GlobalLayout';
-import {
-  Cards,
-  Deck,
-  Landing,
-  NotFound,
-  Setting,
-  SignIn,
-  SignUp,
-} from '../pages';
 import { ROUTE_PATHS } from '../constant/config';
+import { lazy } from 'react';
+
+const Landing = lazy(() =>
+  import('../pages/Landing').then((module) => ({ default: module.Landing }))
+);
+const SignIn = lazy(() =>
+  import('../pages/SignIn').then((module) => ({ default: module.SignIn }))
+);
+const SignUp = lazy(() =>
+  import('../pages/SignUp').then((module) => ({ default: module.SignUp }))
+);
+const Cards = lazy(() =>
+  import('../pages/Cards').then((module) => ({ default: module.Cards }))
+);
+const Deck = lazy(() =>
+  import('../pages/Deck').then((module) => ({ default: module.Deck }))
+);
+const Setting = lazy(() =>
+  import('../pages/Setting').then((module) => ({ default: module.Setting }))
+);
+const NotFound = lazy(() =>
+  import('../pages/NotFound').then((module) => ({ default: module.NotFound }))
+);
 
 /**
  * 참고 자료
