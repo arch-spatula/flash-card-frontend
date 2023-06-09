8a289a003e5f1ec8704b84f8edf57958c9fba032
arch-spatula <shkim516@naver.com>
2023-06-09 16:40:33 +0900

    feat: remove login, logout temporary function #24

--- src/hooks/useLogin/index.ts
+++ src/hooks/useLogin/index.ts
@@ -1,20 +1,10 @@
-import { atom, useAtom } from 'jotai';
+import { useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
-const loggedIn = atom(false);
 const tokenAtom = atomWithStorage('token', '');
 
 export function useLogin() {
-  const [isLoggedIn, setIsLoggedIn] = useAtom(loggedIn);
   const [token, setToken] = useAtom(tokenAtom);
 
-  const login = () => {
-    setIsLoggedIn(true);
-  };
-
-  const logout = () => {
-    setIsLoggedIn(false);
-  };
-
-  return { isLoggedIn, login, logout, token, setToken };
+  return { token, setToken };
 }
