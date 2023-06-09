6cd83897f4cfa15e62537df78ae2d7b739e8204c
arch-spatula <shkim516@naver.com>
2023-06-09 18:42:33 +0900

    feat: add sign out #33

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,5 +1,33 @@
+import { useMutation } from '@tanstack/react-query';
+import { Button } from '../../Components';
+import { signOutAPI } from '../../api/authClient';
+import { useLogin } from '../../hooks';
+import { useNavigate } from 'react-router-dom';
+import { ROUTE_PATHS } from '../../constant/config';
+
 function Setting() {
-  return <div>Setting</div>;
+  const { setToken } = useLogin();
+  const navigate = useNavigate();
+
+  const { mutate, isLoading, error } = useMutation(signOutAPI, {
+    onSuccess: () => {
+      setToken('');
+      navigate(ROUTE_PATHS.SIGN_IN);
+    },
+  });
+
+  const handelSignOut = () => {
+    mutate();
+  };
+
+  return (
+    <div>
+      <h1>Setting</h1>
+      <Button onClick={handelSignOut}>Sign out</Button>
+      {isLoading && '로딩 중입니다.'}
+      {`${error}`}
+    </div>
+  );
 }
 
 export { Setting };
