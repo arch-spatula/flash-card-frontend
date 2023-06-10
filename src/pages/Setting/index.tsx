d6623296ded8b1801b0f10f53e2b73f26ace90e2
arch-spatula <shkim516@naver.com>
2023-06-10 22:57:49 +0900

    refactor: remove useMutation #33

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -1,6 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
 import { Button } from '../../Components';
-import { signOutAPI } from '../../api/authClient';
 import { useLogin } from '../../hooks';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_PATHS } from '../../constant/config';
@@ -9,23 +7,15 @@ function Setting() {
   const { setToken } = useLogin();
   const navigate = useNavigate();
 
-  const { mutate, isLoading, error } = useMutation(signOutAPI, {
-    onSuccess: () => {
-      setToken('');
-      navigate(ROUTE_PATHS.SIGN_IN);
-    },
-  });
-
   const handelSignOut = () => {
-    mutate();
+    setToken('');
+    navigate(ROUTE_PATHS.SIGN_IN);
   };
 
   return (
     <div>
       <h1>Setting</h1>
       <Button onClick={handelSignOut}>Sign out</Button>
-      {isLoading && '로딩 중입니다.'}
-      {`${error}`}
     </div>
   );
 }
