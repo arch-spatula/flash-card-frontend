7a7568ec2b69f57c331f9eefb851d25ca951a818
arch-spatula <shkim516@naver.com>
2023-06-12 17:17:02 +0900

    feat: add conditional height to input helper text
    #35

--- src/Components/Common/Input/Input.style.tsx
+++ src/Components/Common/Input/Input.style.tsx
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 
-export const InputContainer = styled.div`
+export const InputContainer = styled.div<{ hideHelper: boolean }>`
   display: flex;
   flex-direction: column;
   justify-content: left;
   gap: 0.5rem;
+  height: ${(props) => (props.hideHelper ? '2.75rem' : '4.75rem')};
+  flex-grow: 1;
 `;
 
 export const InputWrapper = styled.input`
@@ -12,7 +14,7 @@ export const InputWrapper = styled.input`
   ${(props) => props.theme.fonts.body16Regular}
   padding: 0.5rem 0.75rem;
   box-shadow: 0 0 0 1px ${(props) => props.theme.colors.gray300} inset;
-  border-radius: 0.5rem;
+  border-radius: 0.25rem;
   :hover {
     box-shadow: 0 0 0 1px ${(props) => props.theme.colors.gray400} inset;
   }
