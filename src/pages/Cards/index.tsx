880fbb6725745bd2814c476c30e251df95bb3ad4
arch-spatula <shkim516@naver.com>
2023-06-12 15:25:09 +0900

    feat: add get Cards to Cards page #35

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,5 +1,52 @@
+import { useQuery } from '@tanstack/react-query';
+import { getCardsAPI } from '../../api/cardClient';
+import { PageHeading } from '../../Components';
+import { PulseLoader } from 'react-spinners';
+
 function Cards() {
-  return <div>Cards</div>;
+  const {
+    data: cards,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['cards'],
+    queryFn: getCardsAPI,
+  });
+
+  if (typeof cards === 'string' || error) {
+    return <div>{`${error}`}</div>;
+  }
+
+  return (
+    <div>
+      <PageHeading>Cards</PageHeading>
+      {isLoading ? (
+        <PulseLoader
+          color="#10B981"
+          loading
+          margin={4}
+          size={20}
+          speedMultiplier={0.5}
+        />
+      ) : (
+        <>
+          {cards ? (
+            <ul>
+              {cards.map((card) => (
+                <li key={card._id}>
+                  <h3>{card.question}</h3>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div>
+              <p>카드가 없습니다.</p>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
 }
 
 export default Cards;
