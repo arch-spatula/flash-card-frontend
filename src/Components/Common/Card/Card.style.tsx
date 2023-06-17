b02c5bacc6e3d2662a52d570165b06cf18836014
arch-spatula <shkim516@naver.com>
2023-06-17 09:44:26 +0900

    feat: add Card flip interaction #35

--- src/Components/Common/Card/Card.style.tsx
+++ src/Components/Common/Card/Card.style.tsx
@@ -1,4 +1,29 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
+
+const rotate = keyframes`
+  0% {
+    transform: rotateY(0);
+  }
+  70% {
+    transform: rotateY(200deg);
+  }
+  100% {
+    transform: rotateY(180deg);
+  }
+`;
+
+const rotateInverse = keyframes`
+  0% {
+    transform: rotateY(180deg);
+  }
+  70% {
+    transform: rotateY(-20deg);
+  }
+  100% {
+    transform: rotateY(0);
+  }
+`;
 
 export const Question = styled.h3`
   ${(props) => props.theme.fonts.heading20Bold}
@@ -11,15 +36,37 @@ export const Question = styled.h3`
   word-wrap: break-word;
 `;
 
-export const CardContainer = styled.div`
+/** relative를 적용하기 위해서는 사이즈가 필요함 */
+const CardSize = styled.div`
+  height: 19.5rem;
+  width: 19.5rem;
+`;
+
+export const CardWrapper = styled(CardSize)`
+  position: relative;
+  background-color: ${(props) => props.theme.colors.white};
+`;
+
+export const CardContainer = styled(CardSize)<{ active: boolean }>`
+  position: absolute;
+  left: 0;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 19.5rem;
-  width: 19.5rem;
   box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.12);
   border-radius: 1rem;
   padding: 1.25rem;
+  animation-duration: 0.3s;
+  animation-fill-mode: forwards;
+  backface-visibility: hidden;
+`;
+
+export const CardFrontContainer = styled(CardContainer)`
+  animation-name: ${(props) => (props.active ? rotate : rotateInverse)};
+`;
+
+export const CardBackContainer = styled(CardContainer)`
+  animation-name: ${(props) => (props.active ? rotateInverse : rotate)};
 `;
 
 export const SubmitForm = styled.form`
--- src/Components/Common/Card/Card.tsx
+++ src/Components/Common/Card/Card.tsx
@@ -1,21 +1,45 @@
+import { useState } from 'react';
 import { Button, Input } from '..';
 import { useInput } from '../../../hooks';
-import { CardContainer, Question, SubmitForm } from './Card.style';
+import {
+  CardBackContainer,
+  CardFrontContainer,
+  CardWrapper,
+  Question,
+  SubmitForm,
+} from './Card.style';
 
-export function Card({ question }: Card) {
+/**
+ * @todo 카드 앞면과 뒷면 관심사 분리하기
+ */
+
+export function Card({ question, answer }: Card) {
   const { inputVal, changeInputVal } = useInput();
+  const [active, setActive] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setActive((prev) => !prev);
+  };
+
+  const handleConform = () => {
+    setActive(false);
   };
 
   return (
-    <CardContainer>
-      <Question>{question}</Question>
-      <SubmitForm onSubmit={handleSubmit}>
-        <Input value={inputVal} onChange={changeInputVal} hideHelper />
-        <Button disabled={!inputVal}>제출</Button>
-      </SubmitForm>
-    </CardContainer>
+    <CardWrapper>
+      <CardFrontContainer active={active}>
+        <Question>{question}</Question>
+        <SubmitForm onSubmit={handleSubmit}>
+          <Input value={inputVal} onChange={changeInputVal} hideHelper />
+          <Button disabled={!inputVal}>제출</Button>
+        </SubmitForm>
+      </CardFrontContainer>
+      <CardBackContainer active={active}>
+        <p>{answer}</p>
+        <p>{inputVal}</p>
+        <Button onClick={handleConform}>확인</Button>
+      </CardBackContainer>
+    </CardWrapper>
   );
 }
