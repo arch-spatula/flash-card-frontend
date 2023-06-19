0af56185b40dfeeff68628a38a4253e1ee2b0ab3
arch-spatula <shkim516@naver.com>
2023-06-19 17:43:41 +0900

    feat: init SignUp page #34

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,46 @@
+import { Button, Input, PageHeading } from '../../Components';
+import { useInput } from '../../hooks';
+import { MainContainer, MainWrapper } from './SignUp.style';
+
 function SignUp() {
-  return <div>Sign Up</div>;
+  const { inputVal: email, changeInputVal: changeEmail } = useInput();
+  const { inputVal: password, changeInputVal: changePassword } = useInput();
+  const { inputVal: conformPassword, changeInputVal: changeConformPassword } =
+    useInput();
+
+  const disabled = [
+    email,
+    password,
+    conformPassword,
+    password === conformPassword,
+  ].some((elem) => !elem);
+
+  return (
+    <MainContainer>
+      <MainWrapper>
+        <PageHeading>Sign Up</PageHeading>
+        <Input
+          value={email}
+          onChange={changeEmail}
+          type="email"
+          placeholder="user@email.com"
+        />
+        <Input
+          value={password}
+          onChange={changePassword}
+          type="password"
+          placeholder="8자리 이상 입력해주세요"
+        />
+        <Input
+          value={conformPassword}
+          onChange={changeConformPassword}
+          type="password"
+          placeholder="동일하게 입력해주세요"
+        />
+        <Button disabled={disabled}>회원가입</Button>
+      </MainWrapper>
+    </MainContainer>
+  );
 }
 
 export default SignUp;
