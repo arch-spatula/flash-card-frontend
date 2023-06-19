31895a57bdde7c357f8efeaec22d9c496569ea79
arch-spatula <shkim516@naver.com>
2023-06-19 14:23:13 +0900

    doc: add reason why it is written in this way
    - 왜 관심사에 적절하지 않게 export 하지 않았는지 설명 추가
    - header를 사용하는 이유 추가
    
    #9

--- src/api/AxiosClient.ts
+++ src/api/AxiosClient.ts
@@ -33,6 +33,11 @@ axiosClient.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+/**
+ * - access_token을 갱신하는 함수
+ * - 함수 아래 interceptors만 의존해야 하기 때문에 export하지 않음
+ * - API 명세의 요구에 맞춰 header를 활용하고 refresh token을 설정
+ */
 async function refreshAccessAPI() {
   try {
     const sessionToken = sessionStorage.getItem('sessionToken');
