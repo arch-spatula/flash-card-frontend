62e3fd056b927a8185a6c22a2b8aec128a7e4653
arch-spatula <shkim516@naver.com>
2023-06-21 15:13:43 +0900

    feat: style to menuList #37

--- src/Components/Common/DropdownMenu/DropdownMenu.style.tsx
+++ src/Components/Common/DropdownMenu/DropdownMenu.style.tsx
@@ -1,4 +1,6 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 export const DropdownMenuContainer = styled.div`
   z-index: 2;
@@ -8,8 +10,7 @@ export const DropdownMenuContainer = styled.div`
 export const DropdownOpen = styled.button<{ isOpen: boolean }>`
   all: unset;
   background-color: ${(props) => props.theme.colors.white};
-  border-radius: 0.5rem 0.5rem
-    ${(props) => (props.isOpen ? '0 0' : '0.5rem 0.5rem')};
+  border-radius: 0.5rem;
   width: 3rem;
   height: 3rem;
   display: flex;
@@ -27,4 +28,48 @@ export const DropdownOpen = styled.button<{ isOpen: boolean }>`
 export const MenuList = styled.ul`
   z-index: 2;
   position: absolute;
+  border-radius: 0.5rem;
+  background-color: ${(props) => props.theme.colors.white};
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.12);
+`;
+
+export const MenuItem = styled.li`
+  min-width: 3rem;
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+/** hover, active는 theme provider로 접근하기 때문에 공유할 수 없습니다. */
+const shareItem = css`
+  all: unset;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 0.5rem;
+  padding: 0 1.25rem;
+`;
+
+export const MenuButton = styled.button`
+  ${shareItem}
+  :hover {
+    background-color: ${(props) => props.theme.colors.gray050};
+  }
+  :active {
+    background-color: ${(props) => props.theme.colors.gray100};
+  }
+`;
+
+export const Anchor = styled(Link)`
+  ${shareItem}
+  :hover {
+    background-color: ${(props) => props.theme.colors.gray050};
+  }
+  :active {
+    background-color: ${(props) => props.theme.colors.gray100};
+  }
 `;
