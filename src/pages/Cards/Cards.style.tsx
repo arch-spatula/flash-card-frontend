5284f368b1e9208d438c324a0f2022f6dd895bed
arch-spatula <shkim516@naver.com>
2023-06-21 09:55:37 +0900

    feat: center loader in card page #47

--- src/pages/Cards/Cards.style.tsx
+++ src/pages/Cards/Cards.style.tsx
@@ -1,8 +1,21 @@
 import styled from '@emotion/styled';
 
+export const CardPageContainer = styled.div`
+  min-height: inherit;
+  display: flex;
+  flex-direction: column;
+`;
+
 export const CardContainer = styled.div`
   display: flex;
   gap: 1.75rem;
   flex-wrap: wrap;
+  flex-grow: 1;
+`;
+
+export const LoaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   flex-grow: 1;
 `;
--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,26 +1,34 @@
 import { Card, PageHeading } from '../../Components';
 import { PulseLoader } from 'react-spinners';
-import { CardContainer } from './Cards.style';
+import {
+  CardContainer,
+  CardPageContainer,
+  LoaderContainer,
+} from './Cards.style';
 import { useCards } from '../../hooks';
 
 function Cards() {
-  const { cards, isLoading, error } = useCards();
+  // const { cards, isLoading, error } = useCards();
+  const { cards, error } = useCards();
+  const isLoading = true;
 
   if (typeof cards === 'string' || error) {
     return <div>{`${error}`}</div>;
   }
 
   return (
-    <div>
+    <CardPageContainer>
       <PageHeading>Cards</PageHeading>
       {isLoading ? (
-        <PulseLoader
-          color="#10B981"
-          loading
-          margin={4}
-          size={20}
-          speedMultiplier={0.5}
-        />
+        <LoaderContainer>
+          <PulseLoader
+            color="#10B981"
+            loading
+            margin={4}
+            size={20}
+            speedMultiplier={0.5}
+          />
+        </LoaderContainer>
       ) : (
         <>
           {cards ? (
@@ -36,7 +44,7 @@ function Cards() {
           )}
         </>
       )}
-    </div>
+    </CardPageContainer>
   );
 }
 
