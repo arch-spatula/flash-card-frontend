4e0b0f7aa605a3c1fa09ceb7a3b5dddd266e14fa
arch-spatula <shkim516@naver.com>
2023-06-21 11:35:35 +0900

    feat: add checkAnswer request  #37

--- src/Components/Common/Card/Card.tsx
+++ src/Components/Common/Card/Card.tsx
@@ -8,14 +8,18 @@ import {
   Question,
   SubmitForm,
 } from './Card.style';
+import { useMutation } from '@tanstack/react-query';
+import { updateCardsAPI } from '../../../api/cardClient';
 
 /**
  * @todo 카드 앞면과 뒷면 관심사 분리하기
  */
 
-export function Card({ question, answer }: Card) {
-  const { inputVal, changeInputVal } = useInput();
+export function Card({ question, answer, _id, stackCount }: Card) {
+  const { inputVal, changeInputVal, resetInputVal } = useInput();
   const [active, setActive] = useState(false);
+
+  const { mutate } = useMutation({ mutationFn: updateCardsAPI });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -24,6 +28,24 @@ export function Card({ question, answer }: Card) {
 
   const handleConform = () => {
     setActive(false);
+    resetInputVal();
+
+    if (_id) {
+      const submitDate = new Date();
+      const regex = new RegExp(answer, 'i');
+      const isCorrect = regex.test(inputVal);
+      if (isCorrect) {
+        mutate({
+          id: _id,
+          card: { question, answer, submitDate, stackCount: stackCount + 1 },
+        });
+      } else {
+        mutate({
+          id: _id,
+          card: { question, answer, submitDate, stackCount: 0 },
+        });
+      }
+    }
   };
 
   return (
