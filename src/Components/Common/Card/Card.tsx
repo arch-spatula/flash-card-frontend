171d359673d304db0fe3d2b353608773aafddfbe
arch-spatula <shkim516@naver.com>
2023-06-21 16:29:52 +0900

    feat: add delete card #37

--- src/Components/Common/Card/Card.tsx
+++ src/Components/Common/Card/Card.tsx
@@ -1,17 +1,18 @@
-import { useState } from 'react';
-import { Button, Input } from '..';
+import { useCallback, useState } from 'react';
+import { Button, DropdownMenu, Input } from '..';
 import { useInput } from '../../../hooks';
 import {
   AnswerContainer,
   CardBackContainer,
   CardFrontContainer,
   CardWrapper,
+  MenuWrapper,
   Paragraph,
   Question,
   SubmitForm,
 } from './Card.style';
 import { useMutation } from '@tanstack/react-query';
-import { updateCardsAPI } from '../../../api/cardClient';
+import { deleteCardsAPI, updateCardsAPI } from '../../../api/cardClient';
 
 /**
  * @todo 카드 앞면과 뒷면 관심사 분리하기
@@ -21,9 +22,12 @@ export function Card({ question, answer, _id, stackCount }: Card) {
   const { inputVal, changeInputVal, resetInputVal } = useInput();
   const [active, setActive] = useState(false);
 
-  const { mutate } = useMutation({ mutationFn: updateCardsAPI });
+  const { mutate: updateCard } = useMutation({ mutationFn: updateCardsAPI });
+  const { mutate: deleteCard } = useMutation({ mutationFn: deleteCardsAPI });
 
-  const [isCorrect, setIsCorrect] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [isCorrect, setIsCorrect] = useState(false);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -40,12 +44,12 @@ export function Card({ question, answer, _id, stackCount }: Card) {
     if (_id) {
       const submitDate = new Date();
       if (isCorrect) {
-        mutate({
+        updateCard({
           id: _id,
           card: { question, answer, submitDate, stackCount: stackCount + 1 },
         });
       } else {
-        mutate({
+        updateCard({
           id: _id,
           card: { question, answer, submitDate, stackCount: 0 },
         });
@@ -53,9 +57,18 @@ export function Card({ question, answer, _id, stackCount }: Card) {
     }
   };
 
+  const handleDelete = useCallback(() => {
+    if (_id) deleteCard(_id);
+  }, [deleteCard, _id]);
+
+  const handleEdit = useCallback(() => {
+    setIsEditing(true);
+  }, []);
+
   return (
     <CardWrapper>
       <CardFrontContainer active={active}>
+        <CardSetting handleDelete={handleDelete} handleEdit={handleEdit} />
         <Question>{question}</Question>
         <SubmitForm onSubmit={handleSubmit}>
           <Input
@@ -68,6 +81,7 @@ export function Card({ question, answer, _id, stackCount }: Card) {
         </SubmitForm>
       </CardFrontContainer>
       <CardBackContainer active={active} isCorrect={isCorrect}>
+        <CardSetting handleDelete={handleDelete} handleEdit={handleEdit} />
         <AnswerContainer>
           <Paragraph>정답: {answer}</Paragraph>
           <Paragraph>풀이: {inputVal}</Paragraph>
@@ -75,5 +89,25 @@ export function Card({ question, answer, _id, stackCount }: Card) {
         <Button onClick={handleConform}>확인</Button>
       </CardBackContainer>
     </CardWrapper>
+  );
+}
+
+function CardSetting({
+  handleDelete,
+  handleEdit,
+}: {
+  handleDelete: () => void;
+  handleEdit: () => void;
+}) {
+  return (
+    <MenuWrapper>
+      <DropdownMenu
+        menuItem={[
+          { label: '편집', cb: handleEdit },
+          { label: '삭제', cb: handleDelete },
+        ]}
+        direction="right"
+      />
+    </MenuWrapper>
   );
 }
