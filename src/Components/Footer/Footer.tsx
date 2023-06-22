c0f348dadd0d0953f90c9bc4feafc05792eb500a
arch-spatula <shkim516@naver.com>
2023-06-22 19:32:32 +0900

    feat: add Link with new Tab #53

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,11 @@
-import { FooterContainer } from './Footer.style';
+import { FooterContainer, LinkContainer } from './Footer.style';
 
 export function Footer() {
-  return <FooterContainer></FooterContainer>;
+  return (
+    <FooterContainer>
+      <LinkContainer to="https://github.com/arch-spatula" target="_blank">
+        @arch-spatula
+      </LinkContainer>
+    </FooterContainer>
+  );
 }
