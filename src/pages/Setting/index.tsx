cf06b9eb685c952495e9c4c4f5c1b915a953239e
arch-spatula <shkim516@naver.com>
2023-06-23 14:49:41 +0900

    feat: add loading to logout #54

--- src/pages/Setting/index.tsx
+++ src/pages/Setting/index.tsx
@@ -3,21 +3,39 @@ import { useLogin } from '../../hooks';
 import { useNavigate } from 'react-router-dom';
 import { ROUTE_PATHS } from '../../constant/config';
 import { SettingContainer, SettingWrapper } from './Setting.style';
+import { useEffect, useState } from 'react';
 
 function Setting() {
   const { emptyTokens } = useLogin();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  let timer: NodeJS.Timeout | null = null;
 
   const handelSignOut = () => {
-    emptyTokens();
-    navigate(ROUTE_PATHS.SIGN_IN);
+    setIsLoading(true);
+    const randomDelay = Math.floor(Math.random() * 2 + 1) * 1000;
+
+    timer = setTimeout(() => {
+      setIsLoading(false);
+      emptyTokens();
+      navigate(ROUTE_PATHS.SIGN_IN);
+    }, randomDelay);
   };
+
+  useEffect(() => {
+    return () => {
+      if (timer) clearTimeout(timer);
+    };
+  }, []);
 
   return (
     <SettingContainer>
       <SettingWrapper>
         <PageHeading>Setting</PageHeading>
-        <Button onClick={handelSignOut}>Sign out</Button>
+        <Button onClick={handelSignOut} isLoading={isLoading}>
+          Sign out
+        </Button>
       </SettingWrapper>
     </SettingContainer>
   );
