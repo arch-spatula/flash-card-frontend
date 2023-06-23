a9112b2634cfd32ee313ccc825b66d00191eb73e
arch-spatula <shkim516@naver.com>
2023-06-23 13:20:17 +0900

    feat: add email validation in Sign In #68

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -11,6 +11,7 @@ import {
   Title,
 } from './SignIn.style';
 import { useMutation } from '@tanstack/react-query';
+import { checkEmail } from '../../utils';
 
 /**
  * @todo 화면의 로직과 로그인과 관련된 비즈니스 로직이 강하게 결합되어 있습니다. 결합도를 나추도록 합니다.
@@ -73,7 +74,9 @@ function SignIn() {
     );
   };
 
-  const disabled = [emailValue, passwordValue].some((elem) => !elem);
+  const disabled = [checkEmail(emailValue), passwordValue].some(
+    (elem) => !elem
+  );
 
   return (
     <MainContainer>
