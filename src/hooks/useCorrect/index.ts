864222cb32d057c3aedd35ef5a75411190b786d9
arch-spatula <shkim516@naver.com>
2023-06-26 13:03:49 +0900

    doc: add explanation to useCorrect #37

--- src/hooks/useCorrect/index.ts
+++ src/hooks/useCorrect/index.ts
@@ -2,6 +2,11 @@ import { atom, useAtom } from 'jotai';
 
 const correctAtom = atom(false);
 
+/**
+ * - card 컴포넌트에서만 호출하는 hook
+ * - 정답을 맞추고 틀리고 상태를 공유함
+ * - provider로 분할된 context를 가짐
+ */
 export function useCorrect() {
   const [isCorrect, setIsCorrect] = useAtom(correctAtom);
   return { isCorrect, setIsCorrect };
