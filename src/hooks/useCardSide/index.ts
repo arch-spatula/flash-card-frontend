67e1bd748d315aa16ba07118d2b821250ba2773c
arch-spatula <shkim516@naver.com>
2023-06-26 13:06:32 +0900

    doc: add explanation to useCardSide #37

--- src/hooks/useCardSide/index.ts
+++ src/hooks/useCardSide/index.ts
@@ -5,6 +5,11 @@ type CardSide = 'front' | 'back' | 'edit';
 const cardSideAtom = atom<CardSide>('front');
 const prevCache: { cache: CardSide } = { cache: 'front' };
 
+/**
+ * - card 컴포넌트에서만 호출하는 hook
+ * - 카드의 면(문제, 풀이, 편집)을 state machine pattern으로 공유 함
+ * - provider로 분할된 context를 가짐
+ */
 export function useCardSide() {
   const [cardSide, setCardSide] = useAtom(cardSideAtom);
 
