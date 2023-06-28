02ba81561108a78696d5699683671ecce7cfd810
arch-spatula <shkim516@naver.com>
2023-06-28 11:32:28 +0900

    refactor: swap factory as MapQuery function #49

--- src/utils/auth/protectRoutes/index.ts
+++ src/utils/auth/protectRoutes/index.ts
@@ -2,19 +2,19 @@ import { redirect } from 'react-router-dom';
 import { checkLogin } from '..';
 import { ROUTE_PATHS } from '@/constant/config';
 
-export function protectRoutes(direction: 'signin' | 'cards') {
-  return () => {
-    switch (direction) {
-      case 'cards':
-        if (checkLogin()) throw redirect(ROUTE_PATHS.CARDS);
-        break;
-      case 'signin':
-        if (!checkLogin()) throw redirect(ROUTE_PATHS.SIGN_IN);
-        break;
-      default:
-        break;
-    }
+type Direction = 'signin' | 'cards';
 
+const DirectionMap: { [key in Direction]: () => null } = {
+  cards: () => {
+    if (checkLogin()) throw redirect(ROUTE_PATHS.CARDS);
     return null;
-  };
+  },
+  signin: () => {
+    if (!checkLogin()) throw redirect(ROUTE_PATHS.SIGN_IN);
+    return null;
+  },
+} as const;
+
+export function protectRoutes(direction: Direction) {
+  return DirectionMap[direction];
 }
