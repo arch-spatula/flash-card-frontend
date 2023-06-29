1348dc25f71f51161cd1d0289c9bed6a19aa3d2a
arch-spatula <shkim516@naver.com>
2023-06-29 16:12:12 +0900

    feat: add optimistic delete to card #65

--- src/Components/Common/Card/subcomponents/CardSetting/index.tsx
+++ src/Components/Common/Card/subcomponents/CardSetting/index.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useCardSide } from '../../../../../hooks';
 import { useCallback } from 'react';
 import { DropdownMenu } from '../../..';
@@ -7,7 +7,28 @@ import { deleteCardsAPI } from '../../../../../api/cardClient';
 
 export function CardSetting({ _id }: { _id: string }) {
   const { toggleTo } = useCardSide();
-  const { mutate: deleteCard } = useMutation({ mutationFn: deleteCardsAPI });
+  const queryClient = useQueryClient();
+  const { mutate: deleteCard } = useMutation({
+    mutationFn: deleteCardsAPI,
+    onMutate: async (cardId) => {
+      await queryClient.cancelQueries({ queryKey: ['cards'] });
+
+      const previousCards: Card[] = queryClient.getQueryData(['cards']) ?? [];
+
+      queryClient.setQueryData<Card[]>(['cards'], (oldCards) => {
+        if (oldCards)
+          return [...oldCards].filter((card) => card._id !== cardId);
+        else [];
+      });
+      return { previousCards };
+    },
+    onError: (_err, _cardId, context) => {
+      if (context) queryClient.setQueryData(['cards'], context.previousCards);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['cards'] });
+    },
+  });
 
   const handleDelete = useCallback(() => {
     if (_id) deleteCard(_id);
