efcbb360960f1fe9d4ec13991e9e1f3362054201
arch-spatula <shkim516@naver.com>
2023-07-31 18:12:19 +0900

    test: add sign up success fail by overlap case
    #109

--- src/api/authClient/authClient.test.ts
+++ src/api/authClient/authClient.test.ts
@@ -1,7 +1,26 @@
 import { describe, expect, it } from 'vitest';
+import { signUpAPI } from '.';
 
 describe('authClient - signup', () => {
-  it('should give success for first login', () => {
-    expect(false).toBe(true);
+  it('should fail because of overlapping email', async () => {
+    const res = await signUpAPI({
+      email: 'username@email.com',
+      password: '1234qwer',
+    });
+
+    expect(res?.status).toBe(400);
+    expect(res?.data).toEqual({
+      success: false,
+      msg: 'Error: 이미 가입한 아이디입니다. username@email.com',
+    });
+  });
+
+  it('should give success for non overlapping email', async () => {
+    const res = await signUpAPI({
+      email: 'nouser@email.com',
+      password: '1234qwer',
+    });
+
+    expect(res?.status).toBe(201);
   });
 });
--- src/mocks/handlers/authHandlers.ts
+++ src/mocks/handlers/authHandlers.ts
@@ -1,9 +1,58 @@
 import { rest } from 'msw';
 import { API_URLS, BASE_URL } from '@/constant/config';
 
-export const signUp = rest.post(BASE_URL + API_URLS.SIGN_UP, (_, res, ctx) => {
-  return res(ctx.status(201), ctx.json({ insertedId: '123abc' }));
-});
+type User = {
+  _id: string;
+  email: string;
+  passwordHash: string;
+  passwordSalt: string;
+};
+
+const users: { documents: User[] } = {
+  documents: [
+    {
+      _id: '1234asdf',
+      email: 'username@email.com',
+      passwordHash: '12345678',
+      passwordSalt: '12345678',
+    },
+  ],
+};
+
+export const signUp = rest.post<{ email: string; password: string }>(
+  BASE_URL + API_URLS.SIGN_UP,
+  async (req, res, ctx) => {
+    const { email, password } = await req.json<{
+      email: string;
+      password: string;
+    }>();
+
+    if (!email || !password)
+      return res(
+        ctx.status(400),
+        ctx.json({ success: false, msg: `'이메일 혹은 비밀 번호가 없습니다.'` })
+      );
+
+    if (users.documents.map((user) => user.email).includes(email)) {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          success: false,
+          msg: `Error: 이미 가입한 아이디입니다. ${email}`,
+        })
+      );
+    }
+
+    users.documents.push({
+      _id: 'qwer1234',
+      email,
+      passwordHash: password,
+      passwordSalt: password,
+    });
+
+    return res(ctx.status(201));
+  }
+);
 
 export const signIn = rest.post(BASE_URL + API_URLS.SIGN_IN, (_, res, ctx) => {
   return res(ctx.status(201), ctx.json({ email: 'username@user.com' }));
